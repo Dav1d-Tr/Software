cf2ea5943f3825288153e094f0f3fb24ffbc8a60
DavidTr <davidrozotuberquia@gmail.com>
2023-02-24 16:02:03 -0500

    Final del proyecto

---
+++ ASSETS/JS/app.js
@@ -0,0 +1,11 @@
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function () {
+    nav = document.querySelector(".nav");
+    nav.classList.toggle("active");
+}
+
+window.onload = function () {
+    let contenedor = document.getElementById('cargador');
+    contenedor.style.visibility = 'hidden';
+    contenedor.style.opacity = '0';
+}
